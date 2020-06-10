bacf0e5cd698daf25e65af25009859e4dd2e81a7
jmchugh <joshua.mchugh01@gmail.com>
2020-06-09 20:46:31 -0400

    Initial Commit

---
+++ src/main/java/com/example/demo/MinecraftGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftGameServerApplication.class, args);
+	}
+
+}
