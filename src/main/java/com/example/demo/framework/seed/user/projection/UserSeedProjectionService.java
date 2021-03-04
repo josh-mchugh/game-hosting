c9eb968ca46da5fb5f7f9631285a6caa341c92fc
jmchugh <joshua.mchugh01@gmail.com>
2021-03-03 22:33:39 -0500

    GSH-148 - Refactored user seed service to use projections

---
+++ src/main/java/com/example/demo/framework/seed/user/projection/UserSeedProjectionService.java
@@ -0,0 +1,30 @@
+package com.example.demo.framework.seed.user.projection;
+
+import com.example.demo.framework.seed.user.projection.model.ExistsUserByEmailQuery;
+import com.example.demo.framework.seed.user.projection.model.ExistsUserByEmailResponse;
+import com.example.demo.user.entity.QUserEntity;
+import com.querydsl.jpa.JPQLQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserSeedProjectionService implements IUserSeedProjectionService {
+
+    private final JPQLQueryFactory queryFactory;
+
+    @Override
+    @QueryHandler
+    public ExistsUserByEmailResponse existsByEmail(ExistsUserByEmailQuery query) {
+
+        QUserEntity qUser = QUserEntity.userEntity;
+
+        long count = queryFactory.select(qUser.id)
+                .from(qUser)
+                .where(qUser.email.eq(query.getEmail()))
+                .fetchCount();
+
+        return new ExistsUserByEmailResponse(count >= 1);
+    }
+}
--- src/main/java/com/example/demo/user/projection/IUserProjector.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.user.projection;
-
-public interface IUserProjector {
-
-    boolean existsByEmail(String email);
-}
