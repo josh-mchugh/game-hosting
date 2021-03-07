5f947a0b9b10416d5fcbdd80ee9e1dd0a75d1fe2
jmchugh <joshua.mchugh01@gmail.com>
2021-03-06 20:56:19 -0500

    GSH-148 - Removed unused code from flavor projector

--- src/main/java/com/example/demo/ovh/flavor/projection/FlavorProjector.java
+++ src/main/java/com/example/demo/ovh/flavor/projection/FlavorProjector.java
@@ -4,9 +4,6 @@ import com.example.demo.ovh.flavor.entity.FlavorEntity;
 import com.example.demo.ovh.flavor.entity.QFlavorEntity;
 import com.example.demo.ovh.flavor.entity.mapper.FlavorMapper;
 import com.example.demo.ovh.flavor.entity.model.Flavor;
-import com.example.demo.ovh.flavor.projection.model.FetchFlavorIdByOvhIdProjection;
-import com.example.demo.ovh.flavor.projection.model.FetchFlavorIdByOvhIdQuery;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,20 +13,6 @@ import org.springframework.stereotype.Component;
 public class FlavorProjector implements IFlavorProjector {
 
     private final JPQLQueryFactory queryFactory;
-
-    @Override
-    public FetchFlavorIdByOvhIdProjection fetchFlavorIdByOvhId(FetchFlavorIdByOvhIdQuery query) {
-
-        QFlavorEntity qFlavor = QFlavorEntity.flavorEntity;
-
-        return queryFactory.select(Projections.constructor(
-                    FetchFlavorIdByOvhIdProjection.class,
-                    qFlavor.id
-                ))
-                .from(qFlavor)
-                .where(qFlavor.ovhId.eq(query.getOvhId()))
-                .fetchOne();
-    }
 
     @Override
     public Flavor fetchFlavorByOvhId(String ovhId) {
--- src/main/java/com/example/demo/ovh/flavor/projection/IFlavorProjector.java
+++ src/main/java/com/example/demo/ovh/flavor/projection/IFlavorProjector.java
@@ -1,12 +1,8 @@
 package com.example.demo.ovh.flavor.projection;
 
 import com.example.demo.ovh.flavor.entity.model.Flavor;
-import com.example.demo.ovh.flavor.projection.model.FetchFlavorIdByOvhIdProjection;
-import com.example.demo.ovh.flavor.projection.model.FetchFlavorIdByOvhIdQuery;
 
 public interface IFlavorProjector {
-
-    FetchFlavorIdByOvhIdProjection fetchFlavorIdByOvhId(FetchFlavorIdByOvhIdQuery query);
 
     Flavor fetchFlavorByOvhId(String ovhId);
 }
