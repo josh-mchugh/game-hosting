39b3c5e8aa070b94361f6177a03dd9f1216d9656
jmchugh <joshua.mchugh01@gmail.com>
2021-04-14 23:04:59 -0400

    GSH-148 - Delete AwxInventory findByName as it was not used anywhere

--- src/main/java/com/example/demo/awx/inventory/projection/IAwxInventoryProjector.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.awx.inventory.projection;
-
-import com.example.demo.awx.inventory.entity.model.AwxInventory;
-
-public interface IAwxInventoryProjector {
-
-    AwxInventory findByName(String name);
-}
