d7c9ce8a9a8c7af3d63966236592ea4612294182
jmchugh <joshua.mchugh01@gmail.com>
2021-05-04 23:57:40 -0400

    GSH-144 - Removed Project Create Saga

--- src/main/java/com/example/demo/project/aggregate/saga/IProjectCreatedSagaProjectionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.project.aggregate.saga;
-
-import com.example.demo.project.aggregate.saga.model.FetchAnsibleCredentialQuery;
-import com.example.demo.project.aggregate.saga.model.FetchAnsibleCredentialResponse;
-
-public interface IProjectCreatedSagaProjectionService {
-
-    FetchAnsibleCredentialResponse fetchAnsibleCredential(FetchAnsibleCredentialQuery query);
-}
