63b0971abed9e2a1cb35d1c97b3e224b38f43b61
Josh McHugh <joshua.mchugh01@gmail.com>
2021-05-28 19:18:14 -0400

    GSH-144 - Removed unused create project modal functionality.

--- src/main/java/com/example/demo/web/dashboard/command/service/IDashboardCommandService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.web.dashboard.command.service;
-
-import com.example.demo.web.dashboard.command.service.model.FetchGameIdByGameTypeQuery;
-import com.example.demo.web.dashboard.command.service.model.FetchGameIdByGameTypeResponse;
-
-public interface IDashboardCommandService {
-
-    FetchGameIdByGameTypeResponse getGameId(FetchGameIdByGameTypeQuery query);
-}
