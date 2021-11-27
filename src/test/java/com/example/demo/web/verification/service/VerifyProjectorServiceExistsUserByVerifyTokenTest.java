55ea3540ffe287bb6d340c363679393e599eee4f
Josh McHugh <joshua.mchugh01@gmail.com>
2021-11-26 22:31:06 -0500

    GSH-152 - Refactored Verification User Controller

--- src/test/java/com/example/demo/web/verification/service/VerifyProjectorServiceExistsUserByVerifyTokenTest.java
+++ src/test/java/com/example/demo/web/verification/service/VerifyProjectorServiceExistsUserByVerifyTokenTest.java
@@ -1,9 +1,9 @@
-package com.example.demo.web.verification.projection.service;
+package com.example.demo.web.verification.service;
 
 import com.example.demo.sample.SampleBuilder;
 import com.example.demo.user.entity.model.User;
-import com.example.demo.web.verification.projection.service.model.FetchUserIdByVerificationTokenQuery;
-import com.example.demo.web.verification.projection.service.model.FetchUserIdByVerificationTokenResponse;
+import com.example.demo.web.verification.service.model.FetchUserIdByVerificationTokenQuery;
+import com.example.demo.web.verification.service.model.FetchUserIdByVerificationTokenResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
